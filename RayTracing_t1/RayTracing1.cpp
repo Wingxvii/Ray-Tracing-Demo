80a139b2325cd299e9c9be47a7f197794c40e14a
John <john.wang@uoit.net>
2021-02-14 12:58:47 -0500

    tutorial 2, color per pixel using ray

--- RayTracing_t1/RayTracing1.cpp
+++ RayTracing_t1/RayTracing1.cpp
@@ -13,16 +13,19 @@
 using namespace std;
 using namespace cimg_library;
 
-vec3 color() {
-	
-	//background color blue
-	return vec3(0.5f, 0.7f, 1.0f);
+vec3 color(const ray &r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	//positive shift then halve to lock between 0 and 1
+	float t = 0.5f * (unit_direction.y() + 1.0f);
+
+	//linerally interpolate between white and our original color
+	return (1.0f -t) * vec3(1.0f,1.0f,1.0f) + t * vec3(0.5f,0.7f,1.0f);
 }
 int main()
 {
 	int nx, ny, countRows;
-	nx = 500;
-	ny = 500;
+	nx = 200;
+	ny = 100;
 
 	//create new image 1 plane, 3 colors
 	CImg<unsigned char> img(nx, ny, 1, 3);
@@ -30,12 +33,22 @@ int main()
 	CImgDisplay canvas(img, "RayTracing Test2", 0);
 
 	countRows = 0;
+	vec3 origin(0.0f, 0.0f, 0.0f);
+	vec3 lower_left_corner(-2.0, -1.0f,-1.0f);
+	vec3 horizontal(4.0f, 0.0f, 0.0f);
+	vec3 vertical(0.0f, 2.0f, 0.0f);
+	
 	//go through every pixel
 	for (int j = ny - 1; j >= 0; j--, countRows++) {
 		for (int i = 0; i < nx; i++) {
 			
+			float u = (float)(i) / (float)nx;
+			float v = (float)(j) / (float)ny;
+			//shoot a ray from origin to every pixel
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+
 			//compute color
-			vec3 col = color();
+			vec3 col = color(r);
 
 			//convert into 256 range
 			int ir = int(255.99f * col[0]);
