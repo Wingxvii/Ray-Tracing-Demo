1edc84327dc2d13c8c90344fe68590da0654653f
John <john.wang@uoit.net>
2021-02-14 21:47:41 -0500

    tutorial 5, second sphere and hit table

--- RayTracing_t1/RayTracing1.cpp
+++ RayTracing_t1/RayTracing1.cpp
@@ -13,47 +13,75 @@
 using namespace std;
 using namespace cimg_library;
 
-float hit_sphere(const vec3 & center, float radius, const ray &r) 
-{
-	/* ray collision with a sphere - returns true if ray intersects with the sphere, false otherwise */
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0f* dot(r.direction(), oc);
-	float c = dot(oc,oc) - radius * radius;
-	float discrimiant = b * b - 4.0f * a * c;
 
-	if (discrimiant < 0) {
-		return -1.0f;
+//float hit_sphere(const vec3 & center, float radius, const ray &r) 
+//{
+//	/* ray collision with a sphere - returns true if ray intersects with the sphere, false otherwise */
+//	vec3 oc = r.origin() - center;
+//	float a = dot(r.direction(), r.direction());
+//	float b = 2.0f* dot(r.direction(), oc);
+//	float c = dot(oc,oc) - radius * radius;
+//	float discrimiant = b * b - 4.0f * a * c;
+//
+//	if (discrimiant < 0) {
+//		return -1.0f;
+//	}
+//	else {
+//		return ((-b - sqrt(discrimiant)) / (2.0f * a));
+//	}
+//
+//	//return (discrimiant > 0);
+//}
+
+vec3 color(const ray &r, hittable *world) {
+	hit_record rec;
+	if (world->hit(r, 0.0, FLT_MAX, rec)) {
+		return 0.5f * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
 	else {
-		return ((-b - sqrt(discrimiant)) / (2.0f * a));
-	}
 
-	//return (discrimiant > 0);
+		vec3 unit_direction = unit_vector(r.direction());
+		//positive shift then halve to lock between 0 and 1
+		float t = 0.5f * (unit_direction.y() + 1.0f);
+
+		//linerally interpolate between white and our original color
+		return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+	}
 }
 
-vec3 color(const ray &r) {
-	vec3 sc(0, 0, -1);
-	float t = hit_sphere(sc, 0.5, r);
-	if (t > 0.0f) {
-		vec3 N = unit_vector(r.point_at_parameter(t) - sc);
-		return 0.5f* vec3(N.x() + 1, N.y() + 1, N.z()+1);
+class hittable_list : public hittable {
+public:
+	hittable_list() {}
+	hittable_list(hittable** l, int n) {
+		list = l;
+		list_size = n;
 	}
 
-	vec3 unit_direction = unit_vector(r.direction());
-	//positive shift then halve to lock between 0 and 1
-	t = 0.5f * (unit_direction.y() + 1.0f);
+	virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+		hit_record temp_rec;
+		bool hit_anything = false;
+		double closest_so_far = t_max;
 
-	//linerally interpolate between white and our original color
-	return (1.0f -t) * vec3(1.0f,1.0f,1.0f) + t * vec3(0.5f,0.7f,1.0f);
-}
+		for (int i = 0; i < list_size; i++) {
+			if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
+				hit_anything = true;
+				closest_so_far = temp_rec.t;
+				rec = temp_rec;
+			}
+		}
+		return hit_anything;
+	}
 
+	hittable** list;
+	int list_size;
+
+};
 
 int main()
 {
 	int nx, ny, countRows;
-	nx = 1000;
-	ny = 500;
+	nx = 200;
+	ny = 100;
 
 	//create new image 1 plane, 3 colors
 	CImg<unsigned char> img(nx, ny, 1, 3);
@@ -65,6 +93,12 @@ int main()
 	vec3 lower_left_corner(-2.0, -1.0f,-1.0f);
 	vec3 horizontal(4.0f, 0.0f, 0.0f);
 	vec3 vertical(0.0f, 2.0f, 0.0f);
+
+	hittable* list[2];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5f);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	hittable* world = new hittable_list(list, 2); 
+
 	
 	//go through every pixel
 	for (int j = ny - 1; j >= 0; j--, countRows++) {
@@ -76,7 +110,7 @@ int main()
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
 
 			//compute color
-			vec3 col = color(r);
+			vec3 col = color(r, world);
 
 			//convert into 256 range
 			int ir = int(255.99f * col[0]);
@@ -101,5 +135,4 @@ int main()
 		cimg::wait(20);	//20 ms per frame
 	}
 }
-
 
--- RayTracing_t1/utils.h
+++ RayTracing_t1/utils.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "vec3.h"
+#include "ray.h"
 #define M_PI 3.14159265358979323846264338327950288
-
 
 #include <cstdlib>
 
@@ -17,3 +17,60 @@ vec3 random_in_unit_disk() {
 	return p;
 }
 
+struct hit_record {
+	float t;
+	vec3 p;
+	vec3 normal;
+
+};
+
+class hittable {
+public:
+	virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
+
+class sphere : public hittable {
+public:
+	sphere() {}
+	sphere(vec3 cen, float r) : center(cen), radius(r) {};
+
+	virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+		{
+			/* ray collision with a sphere - returns true if ray intersects with the sphere, false otherwise */
+			vec3 oc = r.origin() - center;
+			float a = dot(r.direction(), r.direction());
+			float b = 2.0f * dot(r.direction(), oc);
+			float c = dot(oc, oc) - radius * radius;
+			float discrimiant = b * b - 4.0f * a * c;
+
+			if (discrimiant < 0) {
+				return false;
+			}
+			else {
+				/* checking for the first solution b^2 -4ac*/
+				float temp = ((-b - sqrt(discrimiant)) / (2.0f * a));
+				if (temp < t_max && temp > t_min) {
+					rec.t = temp;
+					rec.p = r.point_at_parameter(rec.t);
+					rec.normal = (rec.p - center) / radius;
+					return true;
+				}
+
+				/*check for the second solution: b^2 + 4ac*/
+				temp = ((-b + sqrt(discrimiant)) / (2.0f * a));
+				if (temp < t_max && temp > t_min) {
+					rec.t = temp;
+					rec.p = r.point_at_parameter(rec.t);
+					rec.normal = (rec.p - center) / radius;
+					return true;
+				}
+
+				return false;
+			}
+		}
+	}
+
+	/* members */
+	vec3 center;
+	float radius;
+};
